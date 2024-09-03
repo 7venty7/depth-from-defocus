cce81aa766165262350f08d9c24d8c695bb39d45
milesng <miles.c.y.ng@gmail.com>
2024-09-03 14:17:31 +1000

    v1 of variance

--- src/compute_var.py
+++ src/compute_var.py
@@ -9,9 +9,20 @@ def compute_var(if1, if2):
 
     M, N = if1.shape
 
-    var_r[M][N] = [[0]]
+    var_r = np.zeros(M, N)
 
-    for i in range(M):
-        for j in range(N):
-            var_r[i][j] = -(m**2 * n**2) / (math.pi**2 * (N**2 * i**2 + M**2 * j**2))
-            var_r[i][j] *= np.log(if1[i][j] / if2[i][j])
+    for x in range(M):
+        for y in range(N):
+            count = 0
+
+            for i in range(-1, 2):
+                if x + i > 0 and x + i <= M:
+                    for j in range(-1, 2):
+                        if y + j > 0 and y + j <= N:
+                            count += 1
+                            var_r[x][y] = -(M**2 * N**2) / (math.pi**2 * (N**2 * (x+i)**2 + M**2 * (y+j)**2))
+                            var_r[x][y] *= np.log(if1[x][y] / if2[x][y])
+
+            var_r[x][y] /= count
+
+    return var_r
